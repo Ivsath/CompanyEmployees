708073fe3573a66e96d650b95a032f95a159ab0d
Irvin Reyes <reyes.yerp@gmail.com>
2020-02-26 22:55:19 -0600

    Run migrations and add Age property to Employee

--- Entities/Models/Employee.cs
+++ Entities/Models/Employee.cs
@@ -13,6 +13,9 @@ namespace Entities.Models
         [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Age is a required field.")]
+        public int Age { get; set; }
+
         [Required(ErrorMessage = "Position is a required field.")]
         [MaxLength(20, ErrorMessage = "Maximum length for the position is 20 characters")]
         public string Position { get; set; }
