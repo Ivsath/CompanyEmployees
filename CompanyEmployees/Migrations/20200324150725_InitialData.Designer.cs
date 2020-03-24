e877bb47ab3458af74b21b320345b287f1bd3bfc
Irvin Reyes <reyes.yerp@gmail.com>
2020-03-24 09:12:47 -0600

    Add initial data configuration

--- CompanyEmployees/Migrations/20200324150725_InitialData.Designer.cs
+++ CompanyEmployees/Migrations/20200324150725_InitialData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CompanyEmployees.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20200324150725_InitialData")]
+    partial class InitialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,3 +1,4 @@
+using Entities.Configuration;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,12 @@ namespace Entities
         public RepositoryContext(DbContextOptions options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
         }
 
         public DbSet<Company> Companies { get; set; }
