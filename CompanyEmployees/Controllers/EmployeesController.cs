bd51362414eb770604df456d5af3fd002e39d645
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-27 11:47:21 -0500

    Create action to get a single employee for company

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -36,5 +36,27 @@ namespace CompanyEmployees.Controllers
 
             return Ok(employeesDto);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if(company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges: false);
+            if(employeeDb == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+
+            return Ok(employee);
+        }
     }
 }
