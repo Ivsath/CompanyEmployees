aa857a1beac79aebb3f38a83f45b30358cb3f7cf
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-28 10:09:41 -0500

    Create action to delete an employee

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -86,5 +86,29 @@ namespace CompanyEmployees.Controllers
             return CreatedAtRoute("EmployeeById", new { companyId, id =
                 employeeToReturn.Id }, employeeToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if(company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employeeForCompany = _repository.Employee.GetEmployee(companyId, id,
+                trackChanges: false);
+            if(employeeForCompany == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            _repository.Employee.DeleteEmployee(employeeForCompany);
+            _repository.Save();
+
+            return NoContent();
+        }
     }
 }
