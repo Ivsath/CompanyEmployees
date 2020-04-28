db3e729725c31b3d7cc4964e498f2054e11b797a
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-28 10:46:48 -0500

    Add action to update an employee using a put request

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -14,6 +14,7 @@ namespace CompanyEmployees
             CreateMap<Employee, EmployeeDto>();
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
+            CreateMap<EmployeeForUpdateDto, Employee>();
         }
     }
 }
