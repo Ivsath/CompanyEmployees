2cc025a13403b06fe418e6f72d6110acc1dec90d
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-29 11:35:33 -0500

    Add filtering to get the employees by age range

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -33,6 +33,9 @@ namespace CompanyEmployees.Controllers
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId,
             [FromQuery] EmployeeParameters employeeParameters)
         {
+            if(!employeeParameters.ValidAgeRange)
+                return BadRequest("Max age can't be less than min age.");
+            
             var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if(company == null)
             {
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -23,7 +23,10 @@ namespace Repository
             EmployeeParameters employeeParameters,
             bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            var employees = await FindByCondition(
+                    e => e.CompanyId.Equals(companyId) &&
+                         (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge),
+                    trackChanges)
                 .OrderBy(e => e.Name)
                 .ToListAsync();
 
