4d3796f33eafe902ce5f54594ddae97b9b524710
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-30 10:25:16 -0500

    Add XML serialization to the data shaping impl

--- Repository/DataShaping/DataShaper.cs
+++ Repository/DataShaping/DataShaper.cs
@@ -4,6 +4,7 @@ using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using Contracts;
+using Entities.Models;
 
 namespace Repository.DataShaping
 {
@@ -18,14 +19,14 @@ namespace Repository.DataShaping
             Properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
         }
 
-        public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+        public IEnumerable<Entity> ShapeData(IEnumerable<T> entities, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
 
             return FetchData(entities, requiredProperties);
         }
 
-        public ExpandoObject ShapeData(T entity, string fieldsString)
+        public Entity ShapeData(T entity, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
 
@@ -67,10 +68,10 @@ namespace Repository.DataShaping
         }
 
         // Implementation for multiple objects
-        private IEnumerable<ExpandoObject> FetchData(IEnumerable<T> entities,
+        private IEnumerable<Entity> FetchData(IEnumerable<T> entities,
             IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedData = new List<ExpandoObject>();
+            var shapedData = new List<Entity>();
 
             foreach (var entity in entities)
             {
@@ -80,9 +81,9 @@ namespace Repository.DataShaping
 
             return shapedData;
         }
-        private ExpandoObject FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
+        private Entity FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedObject = new ExpandoObject();
+            var shapedObject = new Entity();
 
             foreach (var property in requiredProperties)
             {
