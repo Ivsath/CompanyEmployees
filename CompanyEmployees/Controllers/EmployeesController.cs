c8498f01dc70a13d99fada31efb13463dad63250
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-01 11:07:24 -0500

    Add support for the http head request to the get employees action

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -35,6 +35,7 @@ namespace CompanyEmployees.Controllers
         }
 
         [HttpGet]
+        [HttpHead]
         [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId,
             [FromQuery] EmployeeParameters employeeParameters)
