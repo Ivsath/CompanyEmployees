34021549ab4fc3d83a252ec3ba4d38bd934c155e
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-01 10:46:29 -0500

    Add  options action to the companies controller

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -134,5 +134,13 @@ namespace CompanyEmployees.Controllers
 
             return NoContent();
         }
+
+        [HttpOptions]
+        public IActionResult GetCompaniesOptions()
+        {
+            Response.Headers.Add("Allow", "GET, OPTIONS, POST");
+
+            return Ok();
+        }
     }
 }
